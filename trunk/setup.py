a28a0de64c5e5dbcaab0ff12e7813798bc41f0fe
mario.balibrera <mario.balibrera@481ef212-3f46-0410-8831-316b8a3adfa8>
2008-04-16 03:04:05 +0000

    rel 0.2.0

---
+++ trunk/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+setup(
+    name='rel',
+    version='0.2.0',
+    author='Mario Balibrera',
+    author_email='mario.balibrera@gmail.com',
+    license='MIT License',
+    download_url="http://code.google.com/p/registeredeventlistener/downloads/list",
+    description='Registered Event Listener. Provides standard (pyevent) interface and functionality without external dependencies',
+    long_description='Select preferred event notification methods with initialize([methods in order of preference]). If initialize(...) is not called, methods are tried in default order: pyevent,epoll,poll,select.',
+    packages=[
+        'rel',
+    ],
+    zip_safe = False,
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+)

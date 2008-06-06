cc35c229b772e024d36dc3ce4e617a19113351b9
mario.balibrera <mario.balibrera@481ef212-3f46-0410-8831-316b8a3adfa8>
2008-06-06 22:07:58 +0000

    pyevent GIL hack option, more informative verbose mode output

--- trunk/setup.py
+++ trunk/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='rel',
-    version='0.2.0',
+    version='0.2.1',
     author='Mario Balibrera',
     author_email='mario.balibrera@gmail.com',
     license='MIT License',

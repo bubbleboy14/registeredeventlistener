bcc578a68faf355c175bfa0a7d0ac6f98227a47a
bubbleboy14 <mario.balibrera@gmail.com>
2015-12-02 17:34:14 -0800

    fixed sockio deletion in Registrar.abort()

--- rel/registrar.py
+++ rel/registrar.py
@@ -66,7 +66,7 @@ class Registrar(object):
         self.run_dispatch = False
         for ev_list in self.events.values():
             for sockio in ev_list.values():
-                sockio.sock.close()
+                sockio.delete()
 
     def signal(self,sig,cb,*args):
         return Signal(self,sig,cb,*args)

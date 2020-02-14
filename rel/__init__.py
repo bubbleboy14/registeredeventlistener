47d42b829912a080cead920e06d269ad6fddfb9f
bubbleboy14 <mario.balibrera@gmail.com>
2020-02-13 16:22:24 -0800

    vpush 0.4.3

--- rel/__init__.py
+++ rel/__init__.py
@@ -1,2 +1,2 @@
-__version__ = "0.4.2"
+__version__ = "0.4.3"
 from .rel import override, supported_methods, initialize, read, write, timeout, signal, event, dispatch, loop, report, abort, abort_branch, thread, tick, init, sys, EV_PERSIST, EV_READ, EV_SIGNAL, EV_TIMEOUT, EV_WRITE

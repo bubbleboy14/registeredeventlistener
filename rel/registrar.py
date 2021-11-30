03d0ae253a8e5dd886647bcdf882c1bb7161d34e
bubbleboy14 <mario.balibrera@gmail.com>
2021-11-30 14:09:18 -0800

    Registrar.loop(): faster write events check

--- rel/registrar.py
+++ rel/registrar.py
@@ -73,7 +73,7 @@ class Registrar(object):
                 self.run_dispatch = False
 
     def loop(self):
-        if SLEEP_TURBO and len(list(self.events["write"].keys())):
+        if SLEEP_TURBO and self.events["write"]:
             time.sleep(SLEEP_TURBO)
         else:
             time.sleep(SLEEP_SEC)

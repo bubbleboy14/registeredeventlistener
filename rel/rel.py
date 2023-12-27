a7d747e5763ff38df5d7fe7237b81e2b464333a9
bubbleboy14 <mario.balibrera@gmail.com>
2023-12-26 21:55:43 -0800

    reordered registrar defaults - now prioritize poll, kqueue, and select over epoll and pyevent

--- rel/rel.py
+++ rel/rel.py
@@ -20,7 +20,7 @@ without the need to compile C code, and without breaking the GIL / threading.
 ### registrars
 rel will use the fastest registrar available on your system:
 
-    supported_methods = ['epoll','poll','kqueue','select','pyevent']
+    supported_methods = ['poll','kqueue','select','epoll','pyevent']
 
 The supported_methods[] registrar priority list, as well as other
 settings, can be altered using the (optional) initialize() function:
@@ -88,7 +88,7 @@ running = False
 registrar = None
 threader = None
 verbose = False
-supported_methods = ['epoll','poll','kqueue','select','pyevent']
+supported_methods = ['poll','kqueue','select','epoll','pyevent']
 
 mapping = {
     'select': SelectRegistrar,
@@ -165,7 +165,7 @@ def get_registrar(method):
 
 def initialize(methods=supported_methods,options=()):
     """
-    initialize(methods=['epoll','poll','kqueue','select','pyevent'],options=[])
+    initialize(methods=['poll','kqueue','select','epoll','pyevent'],options=[])
     possible options:
         'verbose' - prints out certain events
         'report' - prints status of non-pyevent registrar every 5 seconds
@@ -192,7 +192,7 @@ def initialize(methods=supported_methods,options=()):
     threader = Thread_Checker('threaded' in options)
     if "report" in options:
         if registrar == pyevent:
-            _display('Reporting disabled in pyevent. Choose epoll, poll, or select to enable reporting.')
+            _display('Reporting disabled in pyevent. Choose poll, kqueue, select, or epoll to enable reporting.')
         else:
             timeout(5,__report)
     return method

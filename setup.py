89288fec4eb391cefec5f3faa9b16d8907a17427
bubbleboy14 <mario.balibrera@gmail.com>
2024-04-26 17:15:18 -0700

    vpush 0.4.9.15

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='rel',
-    version='0.4.9.14',
+    version='0.4.9.15',
     author='Mario Balibrera',
     author_email='mario.balibrera@gmail.com',
     license='MIT License',
